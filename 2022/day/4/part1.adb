5070d27414aaad47ddee1ba1d9a99d2fbdc6bd4b
Laurent Thévenoux <laurent@thevenoux.net>
2022-12-06 21:54:52 +0100

    day4, part1

---
+++ 2022/day/4/part1.adb
@@ -0,0 +1,49 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Part1 is
+   F   : File_Type;
+   Acc : Natural := 0;
+
+   function Process_IDs (IDs : String) return Boolean is
+      First, Last : Positive;
+      Elf1Min, Elf1Max, Elf2Min, Elf2Max : Natural := 0;
+   begin
+      First := 1;
+      Last := First;
+      for C of IDs loop
+         if C = '-' then
+            if Elf1Min = 0 then
+               Elf1Min := Natural'Value (IDs (First .. Last - 1));
+            else
+               Elf2Min := Natural'Value (IDs (First + 1 .. Last - 1));
+            end if;
+            First := Last;
+         end if;
+
+         if C = ',' then
+            Elf1Max := Natural'Value (IDs (First + 1 .. Last - 1));
+            First := Last;
+         end if;
+
+         Last := Last + 1;
+      end loop;
+      Elf2Max := Natural'Value (IDs (First + 1 .. IDs'Last));
+
+      return (Elf1Min <= Elf2Min and Elf1Max >= Elf2Max) or
+               (Elf2Min <= Elf1Min and Elf2Max >= Elf1Max);
+   end Process_IDs;
+
+begin
+   Open (F, In_File, "input");
+   while not End_Of_File (F) loop
+      declare
+         Line : constant String := Get_Line (F);
+      begin
+         if Process_IDs (Line) then
+            Acc := Acc + 1;
+         end if;
+      end;
+   end loop;
+   Close (F);
+   Put_Line (Acc'Image);
+end Part1;
